130f51b26c8ecbeb84c260f1b04660e2ebbca028
danidimic <danidm96@gmail.com>
2020-06-08 11:57:48 +0200

    es 11

--- LSN_Exercise_11/Code/exercise1.py
+++ LSN_Exercise_11/Code/exercise1.py
@@ -9,29 +9,31 @@ from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras import backend as K
 from tensorflow.keras.utils import get_custom_objects
 
-m = 2  #slope
-b = 1  #intersect
-
-sigma = 0.25 #standard deviation of noise
+sigma = 0.75 #standard deviation of noise
 Nepochs = 100 #numero di epoche
 Ntrain = 1000 #numero di valori di test
 
+name = 'sigma' + str(sigma)
+
 def target(x):
-	return m*x + b
+	return 2*x + 1
 
 #generate training inputs
 np.random.seed(0)
-x_valid = np.random.uniform(-1, 1, 10)
-x_valid.sort()
-y_target = target(x_valid)
-x_train = np.random.uniform(-1, 1, Ntrain)
 
-#random noise
-y_train = np.random.normal( target(x_train), sigma ) # actual measures from which we want to guess regression parameters
+#validation data
+x_valid = np.linspace(-1, 1, 15)
+x_valid.sort()
 y_valid = np.random.normal( target(x_valid), sigma )
+#train data
+x_train = np.random.uniform(-1, 1, Ntrain)
+y_train = np.random.normal( target(x_train), sigma ) # actual measures from which we want to guess regression parameters
+#funzione target
+xtarg = np.linspace(-1,1, 25)
+y_target = target(xtarg)
 
 #compose the NN model
-model = tf.keras.Sequential()
+model = Sequential()
 model.add(Dense(1, input_shape=(1,)))
 
 #compile the model choosing optimizer, loss and metrics objects
@@ -46,53 +48,20 @@ history = model.fit(x=x_train, y=y_train,
           validation_data=(x_valid, y_valid))
 
 
-# return weights
-model.get_weights()
+x_predicted = np.linspace(-1.5, 1.5, 100)
+y_predicted = model.predict(x_predicted).reshape(-1)
 
-# evaluate model
-score = model.evaluate(x_valid, y_valid, batch_size=32, verbose=1)
-# print performance
-print()
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-# evaluate model with the exact curve
-score = model.evaluate(x_valid, y_target, batch_size=32, verbose=1)
-# print performance
-print()
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-x_predicted = np.random.uniform(-1, 1, 100)
-y_predicted = model.predict(x_predicted)
-
-'''
-plt.title("Dati di validazione")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.plot(x_valid, y_target)
-plt.scatter(x_valid, y_valid, color='r')
+plt.scatter(x_predicted, y_predicted, color='tab:orange', label='prediction') #predizione NN
+plt.scatter(x_valid, y_valid, color='r', label='validation data') #dati per validazione
+plt.plot(xtarg, target(xtarg), label='target', lw=2.5) #funzione target
 plt.grid(True)
-plt.show()
-
-plt.title("Regressione rete neurale")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.scatter(x_predicted, y_predicted, color='r', label='predizione')
-plt.plot(x_valid, y_target, label='target')
 plt.legend()
-plt.grid(True)
 plt.show()
-'''
 
-y_pred = []
-for i in range (y_predicted.size):
-	y_pred.append(y_predicted[i][0])
+save_model_path='exercise1/model' + name
+model.save(filepath=save_model_path, include_optimizer=True)
 
-#out = 'exercise1/Nepochs' + str(Nepochs) + '.out'
-#out = 'exercise1/Ntrain' + str(Ntrain) + '.out'
-out = 'exercise1/sigma' + str(sigma) + '.out'
-outval = 'exercise1/valid' + str(sigma) + '.out'
-np.savetxt(out, np.column_stack([x_predicted, y_pred]) )
-np.savetxt(outval, np.column_stack([x_valid, y_valid]) )
+
+
+
 
--- LSN_Exercise_11/Code/exercise2.py
+++ LSN_Exercise_11/Code/exercise2.py
@@ -9,9 +9,11 @@ from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras import backend as K
 from tensorflow.keras.utils import get_custom_objects
 
-sigma = 0.25 #standard deviation of noise
+sigma = 0.20 #standard deviation of noise
 Nepochs = 150 #numero di epoche
 Ntrain = 1000 #numero di valori di test
+
+name = ''
 
 def target(x):
 	return 4 - 3*x - 2*x**2 + 3*x**3
@@ -33,10 +35,8 @@ y_target = target(xtarg)
 #compose the NN model
 model = Sequential()
 
-model.add(Dense(units=200, input_dim=1))
-model.add(Activation('relu'))
-model.add(Dense(units=4))
-model.add(Activation('relu'))
+model.add(Dense(units=200, input_dim=1, activation='relu'))
+model.add(Dense(units=45, activation='relu'))
 model.add(Dense(units=1))
 
 #compile the model choosing optimizer, loss and metrics objects
@@ -51,8 +51,8 @@ history = model.fit(x=x_train, y=y_train,
           validation_data=(x_valid, y_valid))
 
 
-x_predicted = np.linspace(-1, 1, 100)
-y_predicted = model.predict(x_predicted)
+x_predicted = np.linspace(-1.5, 1.5, 100)
+y_predicted = model.predict(x_predicted).reshape(-1)
 
 plt.scatter(x_predicted, y_predicted, color='tab:orange', label='prediction') #predizione NN
 plt.scatter(x_valid, y_valid, color='r', label='validation data') #dati per validazione
@@ -60,4 +60,11 @@ plt.plot(xtarg, target(xtarg), label='target', lw=2.5) #funzione target
 plt.grid(True)
 plt.legend()
 plt.show()
+
+save_model_path='exercise2/model' + name
+model.save(filepath=save_model_path, include_optimizer=True)
+
+
+
+
 
