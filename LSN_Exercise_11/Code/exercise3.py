029012d73d1004cc5d533dd13657f25dbe07833a
danidimic <danidm96@gmail.com>
2020-06-08 23:08:24 +0200

    es 11

--- LSN_Exercise_11/Code/exercise3.py
+++ LSN_Exercise_11/Code/exercise3.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+from mpl_toolkits.mplot3d import Axes3D  
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
@@ -11,7 +12,7 @@ from tensorflow.keras.utils import get_custom_objects
 
 a = 1.5
 sigma = 0.20 #standard deviation of noise
-Nepochs = 15 #numero di epoche
+Nepochs = 150 #numero di epoche
 Ntrain = 1000 #numero di valori di test
 Nvalid = 100  #numero di valori di validazione
 
@@ -45,10 +46,9 @@ for i in range(Nvalid):
 
 #compose the NN model
 model = Sequential()
-
-model.add(Dense(units=27, input_dim=2, activation='relu'))
-model.add(Dense(units=18, activation='relu'))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(units=200, input_dim=2, activation='relu'))
+model.add(Dense(units=45, activation='relu'))
+model.add(Dense(1))
 
 #compile the model choosing optimizer, loss and metrics objects
 model.compile(optimizer='sgd', loss='mse', metrics=['mse'])
@@ -61,46 +61,28 @@ history = model.fit(x=x_train, y=y_train,
           shuffle=True, # a good idea is to shuffle input before at each epoch
           validation_data=(x_valid, y_valid))
 
-'''
-x_predicted = np.linspace(-1.5, 1.5, 100)
-y_predicted = model.predict(x_predicted).reshape(-1)
 
-plt.scatter(x_predicted, y_predicted, color='tab:orange', label='prediction') #predizione NN
-plt.scatter(x_valid, y_valid, color='r', label='validation data') #dati per validazione
-plt.plot(xtarg, target(xtarg), label='target', lw=2.5) #funzione target
-plt.grid(True)
-plt.legend()
-plt.show()
-'''
-n = 10
+n = 10000
 x_predicted = np.zeros((n, 2))
 for i in range(n):
 	x_predicted[i,0] = np.random.uniform(-a, a)
 	x_predicted[i,1] = np.random.uniform(-a, a)
 y_predicted = model.predict(x_predicted).reshape(-1)
 
-print(x_predicted)
-print()
-print()
-print(y_predicted)
-print(y_predicted.size())
-
 x = x_predicted[:,0]
 y = x_predicted[:,1]
 z = y_predicted[:]
 
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.gca(projection='3d')
 
-ax.scatter(x, y, z, c='tab:blue')
+ax.scatter(x, y, z, cmap='plasma', c=z, s=5) #prediction della rete neurale
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 plt.show()
 
-
-#save_model_path='exercise3/model' + name
-#model.save(filepath=save_model_path, include_optimizer=True)
+save_model_path='exercise3/model2D'
+model.save(filepath=save_model_path, include_optimizer=True)
 
 
